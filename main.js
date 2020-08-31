97c2f5cf90c0bc3d100c3a2ae07d6f9728e3a072
Kennnnnnji <34367558+Kennnnnnji@users.noreply.github.com>
2020-08-31 20:32:57 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         萌娘百科网址重定向
+// @namespace    https://github.com/Kennnnnnji/Moegirl-Redirect
+// @version      0.1
+// @description  重定向萌娘移动版的网址到桌面版
+// @include      *://mzh.moegirl.org.cn.cc/*
+// @include      *://mzh.moegirl.org.cn/*
+// @author       created by Kennnnnnji
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+document.location.href = document.location.href.replace('mzh.moegirl.org.cn.cc', 'zh.moegirl.org.cn').replace('mzh.moegirl.org.cn', 'zh.moegirl.org.cn');
